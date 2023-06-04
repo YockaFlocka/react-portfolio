673c853809c766906ad8d15fed184a0f1f90db13
Peter Yockey <peterjyockey@gmail.com>
2023-06-04 15:45:25 -0500

    added name to header, filled about me page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import "../styles/About.css";
 
 function About() {
   return (
-    <div>About Me</div>
-  )
+    <div className="home">
+      <div className="about">
+        <h2>Welcome to My Portfolio!</h2>
+        <img src="/Assets/profile-picture.JPG" alt="Profile Pic" />
+        <div className="summary">
+          <p>I am a software developer with a background in Biology and a passion for learning and creating.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -19,11 +19,14 @@ function Header() {
           <AiOutlineMenu /> 
         </button>
       </div>
+
       <div className="links">
-        <Link to="/"> About Me </Link>
-        <Link to="/portfolio"> Portfolio </Link>
-        <Link to="/contact"> Contact </Link>
-        <Link to="/resume"> Resume </Link>
+        <Link to="/" id="name"> Peter Yockey </Link>    
+        <Link to="/" className="navs"> About Me </Link>
+        <Link to="/portfolio" className="navs"> Portfolio </Link>
+        <Link to="/contact" className="navs"> Contact </Link>
+        <Link to="/resume" className="navs"> Resume </Link>
+        <div className="right"></div>
       </div>
     </div>
   )  
