cf175065c7a577f9be7732cdc5909f9c2fae0fe8
Peter Yockey <peterjyockey@gmail.com>
2023-06-05 14:59:08 -0500

    finshed about me page completely

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "../styles/About.css";
+import pic from "../components/assets/profile-picture.JPG"
 
 function About() {
   return (
     <div className="home">
       <div className="about">
         <h2>Welcome to My Portfolio!</h2>
-        <img src="/Assets/profile-picture.JPG" alt="Profile Pic" />
+        <img src={pic} alt="Profile Pic" className="prof-pic" />
         <div className="summary">
-          <p>I am a software developer with a background in Biology and a passion for learning and creating.</p>
+          <p>I am a software developer with a background in Biology, and a passion for learning and creating.</p>
         </div>
       </div>
     </div>
